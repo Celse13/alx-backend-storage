853f70d203ff4b45f2c526f005ed5748aaed8330
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-23 19:47:26 +0200

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
-CREATE TABLE IF NOT EXISTS USERS (
-    id INT AUTO_INCREMENT,
+-- creates a users table if not there
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    PRIMARY KEY (id)
+    name VARCHAR(255)
 );
