47dd7d5eebc15f2cf16ed5c51ea6c29d17634803
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-23 21:14:41 +0200

    Create 11-need_meeting.sql

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+-- operations on students
+
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
