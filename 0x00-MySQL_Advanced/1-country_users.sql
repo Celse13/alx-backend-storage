fef845b4b577dc101ea3ba6594f3bcec8bf2e8a6
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-23 20:12:34 +0200

    Update 1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,8 @@
 -- creating a users table with a country column that only accepts 'US', 'CO', 'TN' values
 
-CREATE TABLE IF NOT EXISTS USERS (
-    id INT AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
-    PRIMARY KEY (id)
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
