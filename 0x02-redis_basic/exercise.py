3046e09296240948ecfbbffa0227bfbf6a8b84e6
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-27 19:07:43 +0200

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,6 +12,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ store """
         key = str(uuid.uuid4())
