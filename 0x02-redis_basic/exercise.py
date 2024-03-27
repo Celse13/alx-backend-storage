33b8425a9677fc2a80b0c704adf674e9ac626df3
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-27 19:17:28 +0200

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,83 +1,87 @@
 #!/usr/bin/env python3
-"""Module documentation"""
+"""doc doc module"""
 
 import redis
 import uuid
 from typing import Union, Callable, Optional
 from functools import wraps
 
-def count_calls(func: Callable) -> Callable:
-    """Decorator to count function calls"""
 
-    @wraps(func)
+def count_calls(method: Callable) -> Callable:
+    """doc doc class"""
+
+    @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """Wrapped function"""
-        func_key = func.__qualname__
-        self._redis.incr(func_key)
-        return func(self, *args, **kwargs)
+        """doc doc class"""
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
 
     return wrapper
 
-def call_history(func: Callable) -> Callable:
-    """Decorator to track function calls"""
-    input_key = func.__qualname__ + ":inputs"
-    output_key = func.__qualname__ + ":outputs"
 
-    @wraps(func)
+def call_history(method: Callable) -> Callable:
+    """doc doc class"""
+    inkey = method.__qualname__ + ":inputs"
+    outkey = method.__qualname__ + ":outputs"
+
+    @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """Wrapped function"""
-        self._redis.rpush(input_key, str(args))
-        result = func(self, *args, **kwargs)
-        self._redis.rpush(output_key, str(result))
-        return result
+        """doc doc class"""
+        self._redis.rpush(inkey, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(outkey, str(res))
+        return res
 
     return wrapper
 
-def replay(func: Callable) -> None:
-    """Function to display function calls"""
-    input_key = "{}:inputs".format(func.__qualname__)
-    output_key = "{}:outputs".format(func.__qualname__)
 
-    inputs = func.__self__._redis.lrange(input_key, 0, -1)
-    outputs = func.__self__._redis.lrange(output_key, 0, -1)
+def replay(method: Callable) -> None:
+    """doc doc class"""
+    input_key = "{}:inputs".format(method.__qualname__)
+    output_key = "{}:outputs".format(method.__qualname__)
 
-    print("{} was invoked {} times:".format(func.__qualname__, len(inputs)))
+    inputs = method.__self__._redis.lrange(input_key, 0, -1)
+    outputs = method.__self__._redis.lrange(output_key, 0, -1)
+
+    print("{} was called {} times:".format(method.__qualname__, len(inputs)))
     for inp, out in zip(inputs, outputs):
         print(
             "{}(*{}) -> {}".format(
-                func.__qualname__, inp.decode("utf-8"), out.decode("utf-8")
+                method.__qualname__, inp.decode("utf-8"), out.decode("utf-8")
             )
         )
 
+
 class Cache:
-    """Class documentation"""
+    """doc doc class"""
 
     def __init__(self):
-        """Initialization method"""
+        """doc doc method"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
     @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Method to save data"""
-        unique_key = str(uuid.uuid4())
-        self._redis.set(unique_key, data)
-        return unique_key
+        """doc doc method"""
+        keyx = str(uuid.uuid4())
+        self._redis.set(keyx, data)
+        return keyx
 
     def get(
-        self, key_str: str, fn: Optional[Callable] = None
+        self, key: str, fn: Optional[Callable] = None
     ) -> Union[str, bytes, int, float]:
-        """Method to retrieve data"""
-        retrieved_value = self._redis.get(key_str)
+        """doc doc method"""
+        value = self._redis.get(key)
         if fn:
-            retrieved_value = fn(retrieved_value)
-        return retrieved_value
+            value = fn(value)
+        return value
 
-    def get_str(self, key_str: str) -> str:
-        """Method to retrieve string data"""
-        return self.get(key_str, fn=str)
+    def get_str(self, key: str) -> str:
+        """doc doc method"""
+        return self.get(key, fn=str)
 
-    def get_int(self, key_str: str) -> int:
-        """Method to retrieve integer data"""
-        return self.get(key_str, fn=int)
+    def get_int(self, key: str) -> int:
+        """doc doc method"""
+        return self.get(key, fn=int)
