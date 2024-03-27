bd1fe558c38377a5c4b1f1455a84e4defc2174e5
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-27 19:05:06 +0200

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,10 +8,12 @@ import uuid
 
 class Cache:
     def __init__(self):
+        """ init func """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ store """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
